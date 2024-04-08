dbafadc262bd268d97b361f1fcf81e99ca6be8e8
Homen <50195844+IamHomen@users.noreply.github.com>
2024-04-08 22:14:07 +0800

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+// pages/index.js
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Welcome to My Next.js Landing Page</h1>
+      <p>This is a basic example of a landing page created with Next.js.</p>
+    </div>
+  );
+};
+
+export default LandingPage;
